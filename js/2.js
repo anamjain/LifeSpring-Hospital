ea1b95a91f1265545c3f161ff8704fd3d3821fb4
anamjain <anamjain129@gmail.com>
2025-08-09 13:44:08 +0530

    first commit

---
+++ js/2.js
@@ -0,0 +1,32 @@
+function updateSpecialization() {
+    const doctorSelect = document.getElementById("doctor");
+    const specializationInput = document.getElementById("specialization");
+
+    const selectedDoctor = doctorSelect.options[doctorSelect.selectedIndex].value;
+}
+
+function submitForm() {
+    const patientName = document.getElementById("patientName").value;
+    const doctor = document.getElementById("doctor").value;
+    const specialization = document.getElementById("specialization").value;
+    const date1 = document.getElementById("date1").value;
+    const date2 = document.getElementById("date2").value;
+
+    const appointmentData = {
+        patientName,
+        doctor,
+        date1,
+        date2
+    };
+    
+    function submitForm() {
+        window.location.href="../pages/3.html";
+    }
+    
+    
+    const existingAppointments = JSON.parse(localStorage.getItem('appointments')) || [];
+    existingAppointments.push(appointmentData);
+    localStorage.setItem('appointments', JSON.stringify(existingAppointments));
+
+    alert('Appointment saved successfully!');
+}
